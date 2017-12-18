4e473b7abbc5777f51d6ada7b1cc3beebab05b11
Junji Hashimoto <junji.hashimoto@gmail.com>
2017-12-18 20:00:31 +0900

    Update README.md

--- npm-sample/index.js
+++ npm-sample/index.js
@@ -1,6 +1,6 @@
 var hugs = require('emhugs');
-var Hugs = new hugs();
-Hugs.init();
-Hugs.run('1+2');
-Hugs.run(':load sort.hs');
-Hugs.run(':main');
+var Hugs = new hugs(); // Create a interpreter-object.
+Hugs.init(); // Initialize interpreter.
+Hugs.run('1+2'); // Run a hugs-command.
+Hugs.run(':load npm-sample/sort.hs'); // Read haskell-code.
+Hugs.run(':main');  // Call main-function of the code.
